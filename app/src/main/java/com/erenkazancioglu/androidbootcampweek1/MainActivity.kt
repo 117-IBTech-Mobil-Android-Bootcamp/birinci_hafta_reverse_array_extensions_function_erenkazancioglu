f52275cd17b783c195c7aee0ecbf91110dc9d5cc
erenkazancioglu <kazancoglueren@gmail.com>
2021-09-01 15:18:34 +0300

    add utilities package and utilities class

--- app/src/main/java/com/erenkazancioglu/androidbootcampweek1/MainActivity.kt
+++ app/src/main/java/com/erenkazancioglu/androidbootcampweek1/MainActivity.kt
@@ -2,7 +2,7 @@ package com.erenkazancioglu.androidbootcampweek1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.lang.RuntimeException
+import com.erenkazancioglu.androidbootcampweek1.utilities.Utilities.Companion.indexAfterReverse
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,33 +10,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val original = arrayListOf<Any>(0, 1, 2, 3, 4, 5,6,7,"eren")
-        val reversed = original.andThenReverse(5)
+        val reversed = original.indexAfterReverse(6)
 
         println(original)
         println(reversed)
-    }
-
-    private fun <E> ArrayList<E>.andThenReverse(index: Int): Any {
-        val reversed = arrayListOf<Any>()
-        try {
-            if(index == 0)
-                return this.reversed()
-            if(index < 0 || index > this.size)
-                throw Exception("index must be positive number and be within the list limits")
-
-            val listSize = this.size
-            var step : Int = 1
-            for ( i in this.indices){
-                if(index >= i){
-                    reversed.add(this.indexOf(i).toString())
-                }
-                else{
-                    reversed.add(this[listSize-step]!!)
-                    step++
-                }
-            }
-        }catch (exception: RuntimeException){
-        }
-        return reversed
     }
 }
